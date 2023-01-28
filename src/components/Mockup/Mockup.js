899f740f76d699fba67e7ec214b8b67c8f1f5d99
Kevin Tran <kevtran@hotmail.com>
2023-01-28 00:48:59 -0800

    Develop basic mockup

---
+++ src/components/Mockup/Mockup.js
@@ -0,0 +1,137 @@
+export default function Mockup() {
+    return (
+        <>
+            {" "}
+            <h6> Mockup</h6>
+            <h1> Hello User</h1>
+            <span>Login</span>
+            <span>Logout</span>
+            <div>
+                <span>Week 3</span>
+                <span>Week 2</span>
+                <span>Week 1</span>
+                <button>Copy Prev Week</button>
+                <button>Add new week</button>
+            </div>
+            <form>
+                <div>
+                    <input placeholder="Day - does not reset on submit" />
+                    <input placeholder="Week - does not reset on submit" />
+                </div>
+                <div>
+                    <input placeholder="Workout - reset" />
+                    <input placeholder="Sets - reset" />
+                    <input placeholder="Reps - reset" />
+                    <input placeholder="Weight - reset" />
+                    <button type="submit">Add</button>
+                </div>
+                <div>Error Message: Please fill out all fields</div>
+            </form>
+            <div>
+                <h3>Day variable</h3>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+            </div>
+            <div>
+                <h3>Day variable</h3>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+            </div>
+            <div>
+                <h3>Day variable</h3>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+                <div>
+                    <span>Workout</span>
+                    <span>Sets</span>
+                    <span>Reps</span>
+                    <span>Weight</span>
+                    <button type="button">Edit</button>
+                    <button type="button">Delete</button>
+                </div>
+            </div>
+        </>
+    );
+}
