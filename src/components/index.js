473e81fb84541a09ad494334b9eb7009fdae2ff5
Kevin Tran <kevtran@hotmail.com>
2023-02-13 15:33:18 -0800

    Move SignUp component to Page

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as SignUp } from "./SignUp/SignUp";
 export { default as SignIn } from "./SignIn/SignIn";
 export { default as Profile } from "./Profile/Profile";
 export { default as Header } from "./Header/Header";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Home } from "./Home/Home";
 export { default as Workouts } from "./Workouts/Workouts";
+export { default as SignUp } from "./SignUp/SignUp";
